f2d66d36577fddebfa69d7d1a7ffede26f106c35
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-04-02 12:54:45 +1100

    Theoretically complete server start

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/LLKennedy/padlock
 go 1.16
 
 require (
-	github.com/LLKennedy/mercury v0.9.0 // indirect
+	github.com/LLKennedy/mercury v0.9.0
 	github.com/LLKennedy/protoc-gen-tsjson v0.5.0
 	github.com/golang/protobuf v1.4.2
+	github.com/google/uuid v1.1.2
 	github.com/miekg/pkcs11 v1.0.3
 	google.golang.org/grpc v1.34.0
 	google.golang.org/protobuf v1.25.0
--- internal/server/handle.go
+++ internal/server/handle.go
@@ -8,17 +8,21 @@ import (
 	"io/fs"
 	"log"
 	"net"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/LLKennedy/mercury"
+	"github.com/LLKennedy/mercury/logs"
 	"github.com/LLKennedy/padlock/api/padlockpb"
 	"github.com/LLKennedy/padlock/padlocklib"
+	"github.com/google/uuid"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
-// Handle is a handle to the Server object
-type Handle struct {
+// handle is a handle to the Server object
+type handle struct {
 	padlockpb.UnimplementedPadlockServer
 	app *padlocklib.Application
 }
@@ -33,9 +37,9 @@ type Config struct {
 	FS       fs.FS
 }
 
-// NewHandle creates a new server handle
-func NewHandle(cfg Config) (*Handle, error) {
-	h := &Handle{
+// Serve creates a new server handle and starts serving traffic
+func Serve(cfg Config) error {
+	h := &handle{
 		app: padlocklib.NewApplication(),
 	}
 	if cfg.FS == nil {
@@ -43,22 +47,22 @@ func NewHandle(cfg Config) (*Handle, error) {
 	}
 	certBytes, err := fs.ReadFile(cfg.FS, cfg.CertFile)
 	if err != nil {
-		return nil, fmt.Errorf("reading cert file %s: %v", cfg.CertFile, err)
+		return fmt.Errorf("reading cert file %s: %v", cfg.CertFile, err)
 	}
 	keyBytes, err := fs.ReadFile(cfg.FS, cfg.KeyFile)
 	if err != nil {
-		return nil, fmt.Errorf("reading key file %s: %v", cfg.KeyFile, err)
+		return fmt.Errorf("reading key file %s: %v", cfg.KeyFile, err)
 	}
 	caData := make([][]byte, len(cfg.CAFiles))
 	for i, caFile := range cfg.CAFiles {
 		caData[i], err = fs.ReadFile(cfg.FS, caFile)
 		if err != nil {
-			return nil, fmt.Errorf("reading ca file %s: %v", caFile, err)
+			return fmt.Errorf("reading ca file %s: %v", caFile, err)
 		}
 	}
 	cert, err := tls.X509KeyPair(certBytes, keyBytes)
 	if err != nil {
-		return nil, fmt.Errorf("parsing cert and key: %v", err)
+		return fmt.Errorf("parsing cert and key: %v", err)
 	}
 	certPool := x509.NewCertPool()
 	for _, ca := range caData {
@@ -67,88 +71,144 @@ func NewHandle(cfg Config) (*Handle, error) {
 			log.Printf("Could not load CA cert(s) from file %s, failed to parse PEM\n", ca)
 		}
 	}
-	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", cfg.Address, cfg.Port))
+	httpListener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", cfg.Address, cfg.Port))
 	if err != nil {
-		return nil, fmt.Errorf("listening on address %s:%d: %v", cfg.Address, cfg.Port, err)
+		return fmt.Errorf("listening on address %s:%d: %v", cfg.Address, cfg.Port, err)
 	}
-	addr := listener.Addr()
+	grpcListener, err := net.Listen("tcp", fmt.Sprintf("%s:0", cfg.Address))
+	if err != nil {
+		return fmt.Errorf("listening on address %s:0: %v", cfg.Address, err)
+	}
+	addr := grpcListener.Addr()
 	tlsConfig := &tls.Config{
 		Certificates: []tls.Certificate{
 			cert,
 		},
 		RootCAs: certPool,
 	}
+	mainCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	srv := grpc.NewServer(grpc.Creds(credentials.NewTLS(tlsConfig)), grpc.StreamInterceptor(h.StreamInterceptor), grpc.UnaryInterceptor(h.UnaryInterceptor))
-	mercury.NewServer()
-	return h, nil
+	proxySrv, err := mercury.NewServer(&padlockpb.UnimplementedPadlockServer{}, h, srv, true)
+	if err != nil {
+		return fmt.Errorf("creating mercury proxy: %v", err)
+	}
+	errChan := make(chan error, 2)
+	go func() {
+		errChan <- proxySrv.Serve(grpcListener)
+		cancel()
+	}()
+	var client *grpc.ClientConn
+	httpSrv := &http.Server{
+		Handler: http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			ctx, cancel := context.WithCancel(r.Context())
+			defer cancel()
+			go func() {
+				<-mainCtx.Done()
+				cancel()
+			}()
+			if client == nil {
+				http.Error(rw, "Server not read", http.StatusPreconditionFailed)
+			}
+			mercury.ProxyRequest(ctx, rw, r, r.URL.Path[1:], client, uuid.New().String(), logs.StdOutLogger{})
+		}),
+		TLSConfig: tlsConfig,
+	}
+	client, err = grpc.Dial(addr.String(), grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
+	if err != nil {
+		log.Printf("First client connection attempt failed (%v), waiting for server startup...\n", err)
+		ticker := time.NewTicker(100 * time.Millisecond)
+	WAIT_LOOP:
+		for err != nil {
+			log.Printf("Client connection attempt failed: %v\n", err)
+			select {
+			case <-mainCtx.Done():
+				log.Println("Encountered error starting service(s) before getting client connection, aborting client connection")
+				break WAIT_LOOP
+			case <-ticker.C:
+				client, err = grpc.Dial(addr.String(), grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
+			}
+		}
+		ticker.Stop()
+	}
+	go func() {
+		errChan <- httpSrv.Serve(httpListener)
+		cancel()
+	}()
+	err = <-errChan
+	go func() {
+		<-errChan
+		close(errChan)
+	}()
+	return err
 }
 
 // UnaryInterceptor intercepts Unary RPCs
-func (h *Handle) UnaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+func (h *handle) UnaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 	return handler(ctx, req)
 }
 
 // StreamInterceptor intercepts Stream RPCs
-func (h *Handle) StreamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+func (h *handle) StreamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 	return handler(srv, ss)
 }
 
 // Hello initiates a session with the application, generating an authentication token
-func (h *Handle) Hello(ctx context.Context, req *padlockpb.AuthHello) (*padlockpb.AuthToken, error) {
+func (h *handle) Hello(ctx context.Context, req *padlockpb.AuthHello) (*padlockpb.AuthToken, error) {
 	return h.UnimplementedPadlockServer.PostHello(ctx, req)
 }
 
 // ApplicationListModules lists modules already connected to the application
-func (h *Handle) ApplicationListModules(ctx context.Context, req *padlockpb.ApplicationListModulesRequest) (*padlockpb.ApplicationListModulesResponse, error) {
+func (h *handle) ApplicationListModules(ctx context.Context, req *padlockpb.ApplicationListModulesRequest) (*padlockpb.ApplicationListModulesResponse, error) {
 	return h.UnimplementedPadlockServer.GetApplicationListModules(ctx, req)
 }
 
 // ApplicationConnect connects a new module to the application
-func (h *Handle) ApplicationConnect(req *padlockpb.ApplicationConnectRequest, stream padlockpb.Padlock_PostApplicationConnectServer) error {
+func (h *handle) ApplicationConnect(req *padlockpb.ApplicationConnectRequest, stream padlockpb.Padlock_PostApplicationConnectServer) error {
 	return h.UnimplementedPadlockServer.PostApplicationConnect(req, stream)
 }
 
 // ModuleListSlots lists the slots on a module
-func (h *Handle) ModuleListSlots(ctx context.Context, req *padlockpb.ModuleListSlotsRequest) (*padlockpb.ModuleListSlotsResponse, error) {
+func (h *handle) ModuleListSlots(ctx context.Context, req *padlockpb.ModuleListSlotsRequest) (*padlockpb.ModuleListSlotsResponse, error) {
 	return h.UnimplementedPadlockServer.GetModuleListSlots(ctx, req)
 }
 
 // ModuleInfo gets info for a specific module
-func (h *Handle) ModuleInfo(ctx context.Context, req *padlockpb.ModuleInfoRequest) (*padlockpb.ModuleInfoResponse, error) {
+func (h *handle) ModuleInfo(ctx context.Context, req *padlockpb.ModuleInfoRequest) (*padlockpb.ModuleInfoResponse, error) {
 	return h.UnimplementedPadlockServer.GetModuleInfo(ctx, req)
 }
 
 // SlotListMechanisms lists the mechanisms available on a slot
-func (h *Handle) SlotListMechanisms(ctx context.Context, req *padlockpb.SlotListMechanismsRequest) (*padlockpb.SlotListMechanismsResponse, error) {
+func (h *handle) SlotListMechanisms(ctx context.Context, req *padlockpb.SlotListMechanismsRequest) (*padlockpb.SlotListMechanismsResponse, error) {
 	return h.UnimplementedPadlockServer.GetSlotListMechanisms(ctx, req)
 }
 
 // SlotInitToken creates the token in the slot
-func (h *Handle) SlotInitToken(ctx context.Context, req *padlockpb.SlotInitTokenRequest) (*padlockpb.SlotInitTokenResponse, error) {
+func (h *handle) SlotInitToken(ctx context.Context, req *padlockpb.SlotInitTokenRequest) (*padlockpb.SlotInitTokenResponse, error) {
 	return h.UnimplementedPadlockServer.PostSlotInitToken(ctx, req)
 }
 
 // SlotOpenSession creates a session on the slot
-func (h *Handle) SlotOpenSession(req *padlockpb.SlotOpenSessionRequest, stream padlockpb.Padlock_PostSlotOpenSessionServer) error {
+func (h *handle) SlotOpenSession(req *padlockpb.SlotOpenSessionRequest, stream padlockpb.Padlock_PostSlotOpenSessionServer) error {
 	return h.UnimplementedPadlockServer.PostSlotOpenSession(req, stream)
 }
 
 // SessionLogin logs into the session at the application level
-func (h *Handle) SessionLogin(ctx context.Context, req *padlockpb.SessionLoginRequest) (*padlockpb.SessionLoginResponse, error) {
+func (h *handle) SessionLogin(ctx context.Context, req *padlockpb.SessionLoginRequest) (*padlockpb.SessionLoginResponse, error) {
 	return h.UnimplementedPadlockServer.PutSessionLogin(ctx, req)
 }
 
 // SessionLogout logs out of the session at the application level
-func (h *Handle) SessionLogout(ctx context.Context, req *padlockpb.SessionID) (*padlockpb.SessionLogoutResponse, error) {
+func (h *handle) SessionLogout(ctx context.Context, req *padlockpb.SessionID) (*padlockpb.SessionLogoutResponse, error) {
 	return h.UnimplementedPadlockServer.PutSessionLogout(ctx, req)
 }
 
 // SessionListObjects lists the objects available in the session
-func (h *Handle) SessionListObjects(req *padlockpb.SessionListObjectsRequest, stream padlockpb.Padlock_GetSessionListObjectsServer) error {
+func (h *handle) SessionListObjects(req *padlockpb.SessionListObjectsRequest, stream padlockpb.Padlock_GetSessionListObjectsServer) error {
 	return h.UnimplementedPadlockServer.GetSessionListObjects(req, stream)
 }
 
 // ObjectListAttributeValues lists values for the requested attributes
-func (h *Handle) ObjectListAttributeValues(req *padlockpb.ObjectListAttributeValuesRequest, stream padlockpb.Padlock_GetObjectListAttributeValuesServer) error {
+func (h *handle) ObjectListAttributeValues(req *padlockpb.ObjectListAttributeValuesRequest, stream padlockpb.Padlock_GetObjectListAttributeValuesServer) error {
 	return h.UnimplementedPadlockServer.GetObjectListAttributeValues(req, stream)
 }
