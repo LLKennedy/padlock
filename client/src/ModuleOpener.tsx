8db042f0981b3faaf4d59dd4062b8365a94eaa9e
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-04-04 19:06:52 +1000

    WIP: Login to session
    Missing PIN input

--- client/src/ModuleOpener.tsx
+++ client/src/ModuleOpener.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 import Module from "./lib/Module"
-import Slot from "./lib/Slot"
-import P11Object from "./lib/P11Object"
 import { ApplicationConnectRequest, AuthHello, AuthToken, ExposedPadlockClient, ModuleInfo } from "@llkennedy/padlock-api";
 import { LoadServerConfig } from "./conf/server";
 import "./ModuleOpener.css";
@@ -43,40 +41,40 @@ export class ModuleOpener extends React.Component<Props, State> {
 			})
 	}
 	render() {
-		if (this.state.info === undefined) return <div id={openerID}>
+		return <div id={openerID}>
 			<h1 id={titleID}>Padlock PKCS#11</h1>
 			<div id={containerID}>
-				{this.state.connecting ? "Connecting..." :
-					[<label key={0} id={labelID}>Module Path</label>,
-					<input key={1} id={inputID} value={this.state.selectedModule} onChange={e => this.setState({ selectedModule: e.target.value })}></input>,
-					<button key={2} id={buttonID} onClick={async () => {
-						this.setState({
-							connecting: true
-						})
-						try {
-							let req = new ApplicationConnectRequest();
-							req.auth = this.state.auth;
-							req.module = this.state.selectedModule;
-							let stream = await this.state.client.ApplicationConnect(req);
-							let update = await stream.Recv();
+				{this.state.info === undefined ?
+					this.state.connecting ? "Connecting..." :
+						[<label key={0} id={labelID}>Module Path</label>,
+						<input key={1} id={inputID} value={this.state.selectedModule} onChange={e => this.setState({ selectedModule: e.target.value })}></input>,
+						<button key={2} id={buttonID} onClick={async () => {
 							this.setState({
-								info: update.info,
-							});
-						} catch (err) {
-							console.error(`Failed to connect to module: ${err}`);
-							window.alert(`Failed to connect to module: ${err}`)
-						} finally {
-							this.setState({
-								connecting: false
+								connecting: true
 							})
-						}
+							try {
+								let req = new ApplicationConnectRequest();
+								req.auth = this.state.auth;
+								req.module = this.state.selectedModule;
+								let stream = await this.state.client.ApplicationConnect(req);
+								let update = await stream.Recv();
+								this.setState({
+									info: update.info,
+								});
+							} catch (err) {
+								console.error(`Failed to connect to module: ${err}`);
+								window.alert(`Failed to connect to module: ${err}`)
+							} finally {
+								this.setState({
+									connecting: false
+								})
+							}
 
-					}}>Connect</button>]
+						}}>Connect</button>]
+					: <Module client={this.state.client} info={this.state.info} auth={this.state.auth} module={this.state.selectedModule} />
 				}
-			</div></div>
-		return <div id={openerID}><div id={containerID}>
-			<Module client={this.state.client} info={this.state.info} />
-		</div></div>
+			</div>
+		</div>
 	}
 }
 
--- client/src/lib/Module.tsx
+++ client/src/lib/Module.tsx
@@ -1,17 +1,90 @@
 import Client from "./Client";
 import React from "react";
-import { ModuleInfo } from "@llkennedy/padlock-api";
+import { AuthToken, ModuleInfo, ModuleListSlotsRequest, SessionID, SessionLoginRequest, SlotID, SlotInfo, SlotOpenSessionRequest } from "@llkennedy/padlock-api";
+import Slot from "./Slot"
+import P11Object from "./P11Object"
 
 export interface Props {
 	client: Client;
 	info: ModuleInfo;
+	module: string;
+	auth?: AuthToken;
 }
 
-export class State { }
+export class State {
+	slots?: SlotInfo[];
+	session?: SessionID;
+	slotID?: SlotID;
+}
 
 export class Module extends React.Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+		this.state = new State();
+	}
+	async componentDidMount() {
+		try {
+			let req = new ModuleListSlotsRequest();
+			req.module = this.props.module;
+			req.auth = this.props.auth;
+			let res = await this.props.client.ModuleListSlots(req);
+			this.setState({
+				slots: res.slots
+			});
+		} catch (err) {
+			const errString = `Failed to list slots: ${err}`;
+			console.error(errString);
+			alert(errString);
+		}
+	}
 	render() {
-		return <div>TODO</div>
+		if (this.state.session !== undefined && this.state.slotID !== undefined) {
+			return <Slot client={this.props.client} />
+		}
+		return <table>
+			<th>ID</th>
+			<th>Description</th>
+			<th>Label</th>
+			<th></th>
+			<tbody>
+				{this.state.slots?.map((val, i) => {
+					return <tr key={i}>
+						<td>{val.id}</td>
+						<td>{val.slotDescription}</td>
+						<td>{val.tokenInfo?.label}</td>
+						<td><button onClick={async e => {
+							try {
+								let req = new SlotOpenSessionRequest();
+								let slotID = new SlotID();
+								slotID.auth = this.props.auth;
+								slotID.module = this.props.module;
+								slotID.slot = val.id;
+								req.id = slotID;
+								req.writeSession = true; // TODO: allow choice here
+								let sessionStream = await this.props.client.SlotOpenSession(req);
+								let session = await sessionStream.Recv();
+								let sessionID = new SessionID();
+								sessionID.uuid = session.uuid;
+								sessionID.auth = this.props.auth;
+								let loginReq = new SessionLoginRequest();
+								loginReq.id = sessionID;
+								loginReq.loginAsSecurityOfficer = false; // TODO: allow choice here
+								loginReq.pin = "1234"; // TODO: input modal
+								await this.props.client.SessionLogin(loginReq);
+								this.setState({
+									session: sessionID,
+									slotID: slotID,
+								});
+							} catch (err) {
+								const errString = `Failed to log into slot: ${err}`;
+								console.error(errString);
+								alert(errString);
+							}
+						}}>Login</button></td>
+					</tr>
+				})}
+			</tbody>
+		</table>
 	}
 }
 
