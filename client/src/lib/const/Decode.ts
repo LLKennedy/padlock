be77072203ff67327fc791162351899cc61f06d2
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-04-11 23:58:41 +1000

    Decode with nulls

--- client/src/lib/const/Decode.ts
+++ client/src/lib/const/Decode.ts
@@ -6,6 +6,9 @@ export function Decode(type: AttributeType, value: Uint8Array) {
 		case AttributeType.CKA_KEY_TYPE:
 			return KeyTypes[AttrToKeyType(value)];
 		default:
+			if (value.length === 0) {
+				return "NULL";
+			}
 			let str = "0x";
 			for (let n of value) {
 				str = str + toHex(n);
